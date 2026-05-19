ff2710c1c8d1b8490e51c3a2053a442e44252eb6
Harish0930 <munagalaharish0930@gmail.com>
2026-05-19 17:36:15 +0530

    update demo.sh

---
+++ demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Sample Shell Script
+
+echo "Welcome to Shell Scripting!"
+
+
+
+# Printing the input
+echo "Hello world!"
+
+# Loop example
+echo "Counting from 1 to 5"
+
+for i in {1..5}
+do
+    echo $i
+done
+
+echo "Script execution completed."
